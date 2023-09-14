d943caf98aa95729f2017d0a6d4d0f423f37c896
Fri5Day <victoramaral951@outlook.com>
2023-09-14 12:31:26 -0300

    Testes internos

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'preview'
-    ? '/GitHub-Repo-Name/'
+    baseUrl: process.env.NODE_ENV === 'production'
+    ? '/bikeramp-front/'
     : '/'
-  }
+    }
+    
