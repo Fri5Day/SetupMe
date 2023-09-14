a9cd8b7a3eb28122250816406c290721daf405f6
Fri5Day <victoramaral951@outlook.com>
2023-09-14 15:22:41 -0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,33 @@
-#!/usr/bin/env sh
-
-set -e
-
-npm run build
-
-cd dist
-
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:Fri5Day/SetupMe.git master:gh-pages
-
-cd -
+// .github/workflows/deploy.yaml
+name: deploy
+on:
+  push:
+    branches:
+      - master
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+      - uses: actions/setup-node@v1
+        with:
+          node-version: '12'
+      - name: setup git auth
+        env:
+          email: 'victoramaral951@outlook.com'
+          username: 'Fri5Day'
+          access_token: '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'
+        run: |
+          git config --global credential.helper store
+          echo "https://${access_token}:x-oauth-basic@github.com" > ~/.git-credentials
+          git config --global user.email $email
+          git config --global user.name $username
+      - name: deploy to gh-pages
+        run: |
+          npm i
+          NODE_ENV=production npm run build
+          cd dist
+          git init
+          git add -A
+          git commit -m "🚀 Deploy id ${GITHUB_RUN_ID}"
+          git push -f "https://github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
